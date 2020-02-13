4d3891731bfd6e334cf3676ddd7696b16e56a5e3
Max Keller <maxkeller@maxs-mbp.speedport_w_724v_09011603_06_006>
2020-02-13 11:43:38 +0100

    vertical_horizontal_lines

---
+++ vertical_horizontal_lines.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt 
+
+plt.axvline(x=2.06, color='k', linestyle='--')
+plt.axvline(x=2.37, color='k', linestyle='--')
+plt.axhline(y=2.37, color='k', linestyle='--')
+
+plt.show()
